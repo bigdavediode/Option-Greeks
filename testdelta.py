27df482d3bd0ea85a314c09855066c759d828224
bigdavediode <bigdavediode2@yahoo.com>
2018-01-06 10:47:32 -0800

    greeks first commit

--- testdelta.py
+++ testdelta.py
@@ -3,7 +3,7 @@ Author: Benjamin Bradford
 
 MIT License
 
-Copyright (c) 2017 backtest-rookies.com
+Copyright (c) 2017 Benjamin Bradford
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -22,6 +22,13 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
+
+# File Contains: Python wrapper code for Davis Edward's code for valuation of American options.
+#
+# This document demonstrates a Python implementation of some option models described in books written by Davis
+# Edwards: "Energy Trading and Investing", "Risk Management in Trading", "Energy Investing Demystified".
+#
+# for backward compatability with Python 2.7
 '''
 
 import backtrader as bt
